0b1737f31c1525d3e06090c8491eef0317c6328b
GleidistonJR <gleidistonjunior@gmail.com>
2025-06-28 15:13:47 -0300

    estrutura

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+include 'base/partials/head.html';
+
+include 'base/partials/header.html';
+
+include 'base/partials/hero_section.html';
+
+include 'base/partials/problema_solucao.html';
+
+include 'base/partials/beneficios_principais.html';
+
+include 'base/partials/footer.html';
+
+?>
